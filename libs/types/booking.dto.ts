ad3678de21ab06be50d27e99ba7f328ca6d4f21b
ilik-sh <i.shyshparonak@gmail.com>
2024-01-18 18:10:40 +0300

    refactor: change booking dto

--- libs/types/booking.dto.ts
+++ libs/types/booking.dto.ts
@@ -1,10 +1,16 @@
 import { UserDto } from "src/aviatickets-submodule/libs/types/user.dto";
 import { UUIDDto } from "src/aviatickets-submodule/libs/types/uuid.dto";
 import { BookingItemDto } from "src/aviatickets-submodule/libs/types/booking-item.dto";
+import { FlightDto } from "./flight.dto";
+import { PassengerDto } from "./passenger.dto";
 
 export interface BookingDto extends UUIDDto {
     status: "Payed" | "Booked" | "Cancelled";
     price: number;
     user: UserDto;
-    bookingItems: BookingItemDto[];
+    routeForward: FlightDto[];
+    routeBackward: FlightDto[];
+    passengers: PassengerDto[];
+    origin: string;
+    destination: string;
 }
