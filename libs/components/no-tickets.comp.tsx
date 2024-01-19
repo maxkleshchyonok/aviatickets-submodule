c34af0f6b1ece6836a237ba7b654b84e49546252
ilik-sh <i.shyshparonak@gmail.com>
2024-01-19 18:10:25 +0300

    feat: add common components

---
+++ libs/components/no-tickets.comp.tsx
@@ -0,0 +1,13 @@
+import Message from "src/aviatickets-submodule/libs/components/message.comp";
+import { FC } from "react";
+
+interface NoTicketsProps {
+  title: string;
+  text?: string
+}
+
+const NoTickets: FC<NoTicketsProps> = (props) => {
+  return <Message {...props} />
+}
+
+export default NoTickets;
