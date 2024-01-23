9c3d987ef45cda994068b6c2836929c2c08e4742
ilik-sh <i.shyshparonak@gmail.com>
2024-01-23 14:39:13 +0300

    feat: move custom text field component in

---
+++ libs/components/text-field.comp.tsx
@@ -0,0 +1,32 @@
+import React, { FC } from "react";
+import { TextField as MUITextField, TextFieldProps } from "@mui/material";
+import { Control, Controller } from "react-hook-form";
+import { camelize } from "app/utils/camelize";
+
+interface CustomTextFieldProps extends TextFieldProps<"standard"> {
+  name: string;
+  control: Control<any, any>;
+}
+
+const TextField: FC<CustomTextFieldProps> = ({ name, control, ...props }) => {
+  return (
+    <Controller
+      control={control}
+      name={camelize(name)}
+      render={({ field }) => (
+        <MUITextField
+          {...props}
+          {...field}
+          name={name}
+          margin="normal"
+          fullWidth
+          id={camelize(name)}
+          autoComplete={camelize(name)}
+          label={name}
+        />
+      )}
+    />
+  );
+};
+
+export default TextField;
